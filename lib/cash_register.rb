2fa058389399217d2591ceec11363c40dbd53135
github username <github email address>
2017-03-19 13:27:49 -0400

    add OO cash register to github

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
 
+	attr_accessor :discount, :total, :items
+
+	def initialize(discount = 0)
+		@total = 0
+		@discount = discount
+		@items = []
+		@last_trans=0
+	end
+
+	def add_item(item, total, quantity = 1)
+		self.total += (total * quantity)
+		quantity.times do
+		@items << item
+		end
+		@last_trans = total
+	end
+
+	def apply_discount
+		if @discount == 0
+			return "There is no discount to apply."
+		end
+		@total *= (100.0-discount)/100
+		return "After the discount, the total comes to $#{@total.round}."
+	end
+
+	def void_last_transaction
+		@total -= @last_trans
+	end
+end
